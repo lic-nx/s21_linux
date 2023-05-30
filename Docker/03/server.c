03425f97eee875d0abb419628252784dac5fc7a7
Nastya <nastya12.11@yandex.ru>
2023-05-30 15:24:14 +0300

    03 done

--- Docker/03/server.c
+++ Docker/03/server.c
@@ -1,9 +1,11 @@
-#include "fcgi_stdio.h"
-#include <stdlib.h>
+#include <fcgi_stdio.h>
 
-int main(void) {
-    while ( FCGI_Accept() >= 0 ) {
-    printf("Content-type: text/html\r\n Hello World! \n");
- }
+int main(int argc, char *argv[])
+{
+  while (FCGI_Accept() >= 0)
+  {
+    printf("Content-Type: text/plain\n\n");
+    printf("Hello World!\n");
+  }
   return 0;
 }
