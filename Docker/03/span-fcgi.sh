e5e645af861107820695877b1999ae6e5b16cf4a
Nastya <nastya12.11@yandex.ru>
2023-05-30 20:04:36 +0300

    03 done

--- Docker/03/span-fcgi.sh
+++ Docker/03/span-fcgi.sh
@@ -1,3 +1,6 @@
-#!/bin/bash                                                                                     
+#!/bin/bash                       
+apt update
+apt upgrade
+apt install -y gcc spawn-fcgi libfcgi-dev                                                            
 gcc -o run_server server.c -lfcgi
 spawn-fcgi -p 8080 -p ./run_server
